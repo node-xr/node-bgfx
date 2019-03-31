171af9a3bcf5303479d467701d4fab51401af0b6
Pras Velagapudi <psigen@gmail.com>
2019-03-30 21:21:19 -0400

    Added some example code.

--- examples/window.js
+++ examples/window.js
@@ -20,6 +20,11 @@ const main = async () => {
   if (!bgfx.init_minimal(info.display, info.window, 640, 480, 0x00000080))
     throw new Error('Failed to initialize BGFX');
 
+  bgfx.set_view_rect_ratio(0, 0, 0, bgfx.BACKBUFFER_RATIO.EQUAL);
+  bgfx.set_view_clear(0x0, 0x0001 | 0x0002, 0xff00ffff, 0.0, 0);
+  bgfx.touch(0x0);
+  bgfx.frame(false);
+
   await sleep(5000);
 
   bgfx.shutdown();
