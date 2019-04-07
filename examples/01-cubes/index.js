53ddcbc9df0c66e3ccb474249b9b09c8fba226f3
Pras Velagapudi <psigen@gmail.com>
2019-04-07 04:41:40 -0400

    Cleaned up the code somewhat.

--- examples/01-cubes/index.js
+++ examples/01-cubes/index.js
@@ -28,60 +28,19 @@ const s_cubeColors = [
 
 // prettier-ignore
 const s_cubeTriList = [
-  0, 1, 2, // 0
-  1, 3, 2,
-  4, 6, 5, // 2
-  5, 6, 7,
-  0, 2, 4, // 4
-  4, 2, 6,
-  1, 5, 3, // 6
-  5, 7, 3,
-  0, 4, 1, // 8
-  4, 5, 1,
-  2, 3, 6, // 10
-  6, 3, 7,
+  0, 2, 1, // 0
+  1, 2, 3,
+  4, 5, 6, // 2
+  5, 7, 6,
+  0, 4, 2, // 4
+  4, 6, 2,
+  1, 3, 5, // 6
+  5, 3, 7,
+  0, 1, 4, // 8
+  4, 1, 5,
+  2, 6, 3, // 10
+  6, 7, 3,
 ];
-
-// prettier-ignore
-const s_cubeTriStrip = [
-  0, 1, 2,
-  3,
-  7,
-  1,
-  5,
-  0,
-  4,
-  2,
-  6,
-  7,
-  4,
-  5
-];
-
-// prettier-ignore
-const s_cubeLineList = [
-  0, 1,
-  0, 2,
-  0, 4,
-  1, 3,
-  1, 5,
-  2, 3,
-  2, 6,
-  3, 7,
-  4, 5,
-  4, 6,
-  5, 7,
-  6, 7,
-];
-
-// prettier-ignore
-const s_cubeLineStrip = [
-  0, 2, 3, 1, 5, 7, 6, 4,
-  0, 2, 6, 4, 5, 7, 3, 1,
-  0
-];
-
-const s_cubePoints = [0, 1, 2, 3, 4, 5, 6, 7];
 
 const fshader = new Uint8Array(
   fs.readFileSync(path.resolve(__dirname, 'shaders/glsl/fs_uniformless.bin')),
@@ -128,14 +87,10 @@ const main = async () => {
     COLOR_0: s_cubeColors,
   }).upload();
 
-  // prettier-ignore
-  const m_ibh = {
-    triList: bgfx.create_index_buffer(new Uint16Array(s_cubeTriList).buffer, BUFFER_NONE),
-    triStrip: bgfx.create_index_buffer(new Uint16Array(s_cubeTriStrip).buffer, BUFFER_NONE),
-    lineList: bgfx.create_index_buffer(new Uint16Array(s_cubeLineList).buffer, BUFFER_NONE),
-    lineStrip: bgfx.create_index_buffer(new Uint16Array(s_cubeLineStrip).buffer, BUFFER_NONE),
-    cubePoint: bgfx.create_index_buffer(new Uint16Array(s_cubePoints).buffer, BUFFER_NONE),
-  };
+  const m_ibh = bgfx.create_index_buffer(
+    new Uint16Array(s_cubeTriList).buffer,
+    BUFFER_NONE,
+  );
 
   let time_start = process.hrtime.bigint();
   let time_last = process.hrtime.bigint();
@@ -165,17 +120,19 @@ const main = async () => {
     // Submit 11x11 cubes.
     for (let yy = 0; yy < 11; ++yy) {
       for (let xx = 0; xx < 11; ++xx) {
+        const time = Number((time_curr - time_start) / 1000000n) / 1e3;
+
         const mtx = mat4.create();
         mat4.translate(mtx, mtx, [(xx - 5) * 3.0, (yy - 5) * 3.0, 0]);
-        mat4.rotateX(mtx, mtx, Number(time_curr - time_start) / 1e9);
-        mat4.rotateY(mtx, mtx, Number(time_curr - time_start) / 1e9);
+        mat4.rotateX(mtx, mtx, xx + time);
+        mat4.rotateY(mtx, mtx, yy + time);
 
         // Set model matrix for rendering.
         bgfx.set_transform(mtx.buffer, 1);
 
         // Set vertex and index buffer.
         bgfx.set_vertex_buffer(0, m_vbh, 0, 0xffffffff);
-        bgfx.set_index_buffer(m_ibh.triList, 0, 0xffffffff);
+        bgfx.set_index_buffer(m_ibh, 0, 0xffffffff);
 
         // Set render states.
         bgfx.set_state(bgfx.STATE_DEFAULT, 0);
