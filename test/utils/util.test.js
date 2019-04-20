843c4588338f79515c76ca981c0abdd070ccb8fd
Pras Velagapudi <psigen@gmail.com>
2019-04-20 11:07:34 -0400

    Added a sleep utility method.

--- test/utils/util.test.js
+++ test/utils/util.test.js
@@ -1,4 +1,4 @@
-const { checksumFile } = require('../../lib/utils');
+const { checksumFile, sleep } = require('../../lib/utils');
 const path = require('path');
 
 const testPath = path.resolve(__dirname, 'payload.txt');
@@ -8,4 +8,23 @@ describe('checksumFile', () => {
     const md5 = await checksumFile(testPath, 'md5');
     expect(md5).toEqual('851f29eb40bc9f8367f4d43759f87f79');
   });
+});
+
+describe('sleep', () => {
+  const delayMs = 100;
+
+  it('terminates if given no arguments', async () => {
+    await sleep();
+  }, 10);
+
+  it(
+    'waits the right amount of time',
+    async () => {
+      const start = process.hrtime.bigint();
+      await sleep(delayMs);
+      const end = process.hrtime.bigint();
+      expect(Number(end - start)).toBeGreaterThan(delayMs);
+    },
+    delayMs + 10,
+  );
 });
