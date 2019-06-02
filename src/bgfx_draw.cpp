e37bdc3e551508c52de8e6c33b74d88e7f855271
Pras Velagapudi <psigen@gmail.com>
2019-06-01 22:37:18 -0400

    Another step towards a unified drawcall.

--- src/bgfx_draw.cpp
+++ src/bgfx_draw.cpp
@@ -1,18 +1,49 @@
 #include "bgfx_draw.hpp"
 #include "bgfx_converters.hpp"
 
-constexpr uintptr_t IS_DYNAMIC_MASK = (1<<31);
+constexpr uintptr_t IS_DYNAMIC_MASK = (1 << 31);
 
-constexpr bool is_dynamic_handle(uintptr_t handle) {
+constexpr bool is_dynamic_handle(uintptr_t handle)
+{
   return (handle & IS_DYNAMIC_MASK);
 }
 
-template<typename T>
-constexpr T to_handle(uintptr_t handle) {
-  T result;
+template <typename T>
+constexpr T to_handle(uintptr_t handle)
+{
+  T result{};
   result.idx = handle;
   return result;
 }
+
+namespace wrap
+{
+
+template <>
+inline bgfx_index_buffer_args_t decode(napi_env env, napi_value value)
+{
+  bgfx_index_buffer_args_t result;
+  ok(napi_get_value_external(env, value, reinterpret_cast<void **>(&result)));
+  return result;
+}
+
+template <>
+inline bgfx_vertex_buffer_args_t decode(napi_env env, napi_value value)
+{
+  bgfx_vertex_buffer_args_t result;
+  ok(napi_get_value_external(env, value, reinterpret_cast<void **>(&result)));
+  return result;
+}
+
+template <>
+inline bgfx_uniform_args_t decode(napi_env env, napi_value value)
+{
+  bgfx_uniform_args_t result;
+  ok(napi_get_value_external(env, value, reinterpret_cast<void **>(&result)));
+  return result;
+}
+
+} // namespace wrap
 
 void bgfx_draw(bgfx_drawcall_t drawcall)
 {
@@ -71,10 +102,10 @@ bgfx_drawcall_t decode(napi_env env, napi_value value)
   result.xform = wrap::decode_property<bgfx_mat4_t *>(env, value, "xform");
   result.state = wrap::decode_property<uint64_t>(env, value, "state");
   result.rgba = wrap::decode_property<uint32_t>(env, value, "rgba");
-  result.depth = wrap::decode_property<uint32_t>(env, value, "rgba");
-  result.index;
-  result.vertex;
-  result.uniforms;
+  result.depth = wrap::decode_property<uint32_t>(env, value, "depth");
+  result.index = wrap::decode_property<bgfx_index_buffer_args_t>(env, value, "index");
+  result.vertex = wrap::decode_property<bgfx_vertex_buffer_args_t>(env, value, "vertex");
+  result.uniforms = wrap::decode_property<std::vector<bgfx_uniform_args_t>>(env, value, "vertex");
 
   return result;
 }
